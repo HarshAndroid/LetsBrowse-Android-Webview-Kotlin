c16440af71bf101b999fefb1cb899d6c40da9f7d
AndroidHarsh <rajpurohitharsh2020@gmail.com>
2022-04-21 19:14:19 +0530

    More Features.

--- app/src/main/java/com/harshrajpurohit/letsbrowse/MainActivity.kt
+++ app/src/main/java/com/harshrajpurohit/letsbrowse/MainActivity.kt
@@ -2,20 +2,33 @@ package com.harshrajpurohit.letsbrowse
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.drawable.ColorDrawable
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
 import android.os.Bundle
+import android.print.PrintAttributes
+import android.print.PrintJob
+import android.print.PrintManager
+import android.view.Gravity
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import com.harshrajpurohit.letsbrowse.databinding.ActivityMainBinding
+import com.harshrajpurohit.letsbrowse.databinding.MoreFeaturesBinding
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private var printJob: PrintJob? = null
 
     companion object{
         var tabsList: ArrayList<Fragment> = ArrayList()
@@ -32,12 +45,11 @@ class MainActivity : AppCompatActivity() {
 
         binding.myPager.adapter = TabsAdapter(supportFragmentManager, lifecycle)
         binding.myPager.isUserInputEnabled = false
-
+        initializeView()
     }
 
     @SuppressLint("NotifyDataSetChanged")
     override fun onBackPressed() {
-
         var frag:BrowseFragment? = null
         try {
             frag = tabsList[binding.myPager.currentItem] as BrowseFragment
@@ -89,4 +101,62 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun initializeView(){
+        binding.settingBtn.setOnClickListener {
+            var frag:BrowseFragment? = null
+            try {
+                frag = tabsList[binding.myPager.currentItem] as BrowseFragment
+            }catch (e:Exception){}
+
+            val view = layoutInflater.inflate(R.layout.more_features, binding.root, false)
+            val dialogBinding = MoreFeaturesBinding.bind(view)
+
+            val dialog = MaterialAlertDialogBuilder(this).setView(view).create()
+
+            dialog.window?.apply {
+                attributes.gravity = Gravity.BOTTOM
+                attributes.y = 50
+                setBackgroundDrawable(ColorDrawable(0xFFFFFFFF.toInt()))
+            }
+            dialog.show()
+
+            dialogBinding.backBtn.setOnClickListener {
+                onBackPressed()
+            }
+
+            dialogBinding.forwardBtn.setOnClickListener {
+                frag?.apply {
+                    if(binding.webView.canGoForward())
+                        binding.webView.goForward()
+                }
+            }
+
+            dialogBinding.saveBtn.setOnClickListener {
+                if(frag != null)
+                    saveAsPdf(web = frag.binding.webView)
+                else Snackbar.make(binding.root, "First Open A WebPage\uD83D\uDE03", 3000).show()
+            }
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        printJob?.let {
+            when{
+                it.isCompleted -> Snackbar.make(binding.root, "Successful -> ${it.info.label}", 4000).show()
+                it.isFailed -> Snackbar.make(binding.root, "Failed -> ${it.info.label}", 4000).show()
+            }
+        }
+    }
+
+    private fun saveAsPdf(web: WebView){
+        val pm = getSystemService(Context.PRINT_SERVICE) as PrintManager
+
+        val jobName = "${URL(web.url).host}_${SimpleDateFormat("HH:mm d_MMM_yy", Locale.ENGLISH)
+            .format(Calendar.getInstance().time)}"
+        val printAdapter = web.createPrintDocumentAdapter(jobName)
+        val printAttributes = PrintAttributes.Builder()
+        printJob = pm.print(jobName, printAdapter, printAttributes.build())
+    }
 }
